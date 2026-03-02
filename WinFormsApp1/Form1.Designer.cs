2c42b124f41710290b74d3f01ddeacc28e793d6d
ra2557097 <ra2557097@LDI311>
2026-03-02 11:57:04 -0300

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Alt = new Label();
+            Pes = new Label();
+            lblresultado = new Label();
+            lblClassif = new Label();
+            numAltura = new NumericUpDown();
+            numPeso = new NumericUpDown();
+            M = new Label();
+            Kg = new Label();
+            Calcular = new Button();
+            ((System.ComponentModel.ISupportInitialize)numAltura).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numPeso).BeginInit();
+            SuspendLayout();
+            // 
+            // Alt
+            // 
+            Alt.AutoSize = true;
+            Alt.Font = new Font("Segoe UI", 18F);
+            Alt.Location = new Point(77, 86);
+            Alt.Name = "Alt";
+            Alt.Size = new Size(77, 32);
+            Alt.TabIndex = 0;
+            Alt.Text = "Altura";
+            // 
+            // Pes
+            // 
+            Pes.AutoSize = true;
+            Pes.Font = new Font("Segoe UI", 18F);
+            Pes.Location = new Point(77, 143);
+            Pes.Name = "Pes";
+            Pes.Size = new Size(63, 32);
+            Pes.TabIndex = 1;
+            Pes.Text = "Peso";
+            // 
+            // lblresultado
+            // 
+            lblresultado.AutoSize = true;
+            lblresultado.Font = new Font("Segoe UI", 18F);
+            lblresultado.Location = new Point(62, 298);
+            lblresultado.Name = "lblresultado";
+            lblresultado.Size = new Size(0, 32);
+            lblresultado.TabIndex = 2;
+            // 
+            // lblClassif
+            // 
+            lblClassif.AutoSize = true;
+            lblClassif.Font = new Font("Segoe UI", 18F);
+            lblClassif.Location = new Point(62, 374);
+            lblClassif.Name = "lblClassif";
+            lblClassif.Size = new Size(0, 32);
+            lblClassif.TabIndex = 3;
+            // 
+            // numAltura
+            // 
+            numAltura.DecimalPlaces = 2;
+            numAltura.Font = new Font("Segoe UI", 18F);
+            numAltura.Location = new Point(174, 84);
+            numAltura.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            numAltura.Name = "numAltura";
+            numAltura.Size = new Size(181, 39);
+            numAltura.TabIndex = 4;
+            numAltura.ValueChanged += numAltura_ValueChanged;
+            // 
+            // numPeso
+            // 
+            numPeso.DecimalPlaces = 2;
+            numPeso.Font = new Font("Segoe UI", 18F);
+            numPeso.Location = new Point(174, 143);
+            numPeso.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            numPeso.Name = "numPeso";
+            numPeso.Size = new Size(181, 39);
+            numPeso.TabIndex = 5;
+            // 
+            // M
+            // 
+            M.AutoSize = true;
+            M.Font = new Font("Segoe UI", 18F);
+            M.Location = new Point(398, 91);
+            M.Name = "M";
+            M.Size = new Size(36, 32);
+            M.TabIndex = 6;
+            M.Text = "M";
+            // 
+            // Kg
+            // 
+            Kg.AutoSize = true;
+            Kg.Font = new Font("Segoe UI", 18F);
+            Kg.Location = new Point(398, 151);
+            Kg.Name = "Kg";
+            Kg.Size = new Size(42, 32);
+            Kg.TabIndex = 7;
+            Kg.Text = "Kg";
+            // 
+            // Calcular
+            // 
+            Calcular.Enabled = false;
+            Calcular.Font = new Font("Segoe UI", 18F);
+            Calcular.Location = new Point(127, 213);
+            Calcular.Name = "Calcular";
+            Calcular.Size = new Size(266, 41);
+            Calcular.TabIndex = 8;
+            Calcular.Text = "Calcular";
+            Calcular.UseVisualStyleBackColor = true;
+            Calcular.Click += Calcular_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(475, 450);
+            Controls.Add(Calcular);
+            Controls.Add(Kg);
+            Controls.Add(M);
+            Controls.Add(numPeso);
+            Controls.Add(numAltura);
+            Controls.Add(lblClassif);
+            Controls.Add(lblresultado);
+            Controls.Add(Pes);
+            Controls.Add(Alt);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numAltura).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numPeso).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Alt;
+        private Label Pes;
+        private Label lblresultado;
+        private Label lblClassif;
+        private NumericUpDown numAltura;
+        private NumericUpDown numPeso;
+        private Label M;
+        private Label Kg;
+        private Button Calcular;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            double alt, peso, imc;
+            alt = (double)numAltura.Value;
+            peso = (double)numPeso.Value;
+
+
+            imc = peso / Math.Pow(alt, 2);
+
+            lblresultado.Text = $"IMC: {imc:F2} kg/m²";
+            lblClassif.Text = Classificar(imc);
+        }
+           
+
+        string Classificar(double imc)
+        {
+            if (imc < 18.5)
+                return "Abaixo do Peso";
+            if (imc < 25)
+                return "Peso Ideal";
+            if (imc < 30)
+                return "Obesidade I";
+            if (imc < 35)
+                return "Obesidade II";
+             if (imc < 40)
+                return "Obesidade III";
+            return "Obesidade mórbida";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numAltura_ValueChanged(object sender, EventArgs e)
+        {
+            decimal a = numAltura.Value;
+           /* if (a != 0)
+                Calcular.Enabled = true;
+            else
+                Calcular.Enabled = false;
+           */
+           Calcular.Enabled = (a != 0);
+        }
+    }
+}
